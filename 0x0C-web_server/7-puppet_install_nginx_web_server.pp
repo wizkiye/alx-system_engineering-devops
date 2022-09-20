9cb1e79fcf55bcac8cbdd374315dfb25adc69f7f
Kidus <wizkiye@gmail.com>
2022-09-20 23:09:24 +0300

    7-puppet_install_nginx_web_server.pp

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,20 +1,43 @@
-# Puppet manifest to install nginx
-package { 'nginx':
-  ensure => installed,
+# configure nginx server to listen to port 80
+# with permanent redirect for /redirect_me and handle
+# custom error 404 page
+
+exec {'apt-get-update':
+  command => '/usr/bin/apt-get update'
 }
 
-file_line { 'aaaaa':
-  ensure => 'present',
-  path   => '/etc/nginx/sites-available/default',
-  after  => 'listen 80 default_server;',
-  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
+package {'nginx':
+  ensure  => 'installed',
+  require => Exec['apt-get-update'],
 }
 
-file { '/var/www/html/index.html':
+file {'/var/www/html/index.html':
+  ensure  => 'present',
   content => 'Holberton School',
+  require => Package['nginx']
 }
 
-service { 'nginx':
-  ensure  => running,
-  require => Package['nginx'],
+file_line {'server_name _;':
+  path    => '/etc/nginx/sites-available/default',
+  line    => "\n\tlocation /redirect_me {\n\t\trewrite ^/redirect_me(.*)$ https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;\n\t}",
+  after   => 'server_name _;',
+  require => Package['nginx']
+}
+
+file {'/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page",
+  require => Package['nginx']
+}
+
+file_line {'server_name _; ':
+  path    => '/etc/nginx/sites-enabled/default',
+  line    => "\n\terror_page 404 /404.html;\n\tlocation = /404.html {\n\t\troot /var/www/html;\n\t\tinternal;\n\t}",
+  after   => 'server_name _;',
+  require => Package['nginx']
+}
+
+service {'nginx':
+  ensure  => 'running',
+  require => Package['nginx']
 }
