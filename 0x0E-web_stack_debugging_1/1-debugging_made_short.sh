68b47b5a584e5991a94add8a8d3dfba393c62793
Kidus <wizkiye@gmail.com>
2022-10-01 19:58:46 +0300

    1-debugging_made_short.sh

--- 0x0E-web_stack_debugging_1/1-debugging_made_short.sh
+++ 0x0E-web_stack_debugging_1/1-debugging_made_short.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
-# script that automates nginx server to run at port 80
-sed -i s/8080/80/ /etc/nginx/sites-enabled/default
+# Command to make an nginx server listen on port 80, shorter version
+sed -i 's/8080/80/g' /etc/nginx/sites-enabled/default
 pkill -1 nginx
